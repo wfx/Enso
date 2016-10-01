73df9704a5151979f123b02868021804e28b2cf6
wfx <wolfgang.morawetz@gmail.com>
2016-10-01 23:08:07 +0200

    base builds

---
+++ ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get -y install \
+wget curl subversion git cvs bzr build-essential \
+autoconf automake autopoint gettext pkg-config libtool autotools-dev \
+zlib1g-dev libpng12-dev libjpeg-dev libgif-dev libtiff4-dev \
+libfreetype6-dev libxcb-shape0-dev libx11-dev libxp-dev libxtst-dev \
+libxext-dev libxrender-dev libxinerama-dev libxcb-keysyms1-dev \
+libxcursor-dev libxrandr-dev libxdamage-dev libxcomposite-dev \
+libxss-dev libssl-dev libcurl4-openssl-dev libfribidi-dev \
+libdbus-1-dev libpam0g-dev libxml2-dev libxslt1-dev libltdl3-dev \
+libgl1-mesa-dev libglu1-mesa-dev python-dev librsvg2-dev \
+libtag1-dev libtagc0-dev libxine-dev libsqlite3-dev fontconfig \
+libfontconfig-dev liblua5.1-dev libc-ares-dev libexif-dev \
+libcanberra-dev libudev-dev libvlc-dev libsndfile1-dev libmount-dev \
+libblkid-dev check libpulse-dev
