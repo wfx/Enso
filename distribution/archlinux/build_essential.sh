1419f70301cb4a237151f5a9e38407c37b445f5f
wfx <wolfgang.morawetz@gmail.com>
2016-10-04 21:04:36 +0200

    initial distribution work

---
+++ distribution/archlinux/build_essential.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pacman -Syu \
+base-devel check libpam freetype2 libpng libjpeg zlib libdbus luajit libx11 libxcursor libxrender \
+libxrandr libxfixes libxdamage libxcomposite libxss libxp libxext libxinerama libxkbfile \
+libxtst libxcb libxcb-shape libxcb-keysyms1 libpulse libsndfile libudev libblkid libmount \
+libutil-linux libgstreamer vlc libtiff giflib curl openssl libspectre poppler librsvg libraw \
+libxine bullet wget
