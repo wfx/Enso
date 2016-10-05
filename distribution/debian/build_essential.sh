11aeec971c2c00ebbda724dbfecd686b00374a0c
wfx <wolfgang.morawetz@gmail.com>
2016-10-05 23:57:14 +0200

    ... hopefully now

--- distribution/debian/build_essential.sh
+++ distribution/debian/build_essential.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-apt-get -y \
-make gcc bison flex gawk subversion automake autoconf doxygen\
-check autotools-dev autoconf-archive autopoint libtool gettext \
-libpam0g-dev libfreetype6-dev libpng12-dev zlib1g-dev  libjpeg-dev\
-libdbus-1-dev luajit libluajit-5.1-dev libx11-dev libxcursor-dev\
-libxrender-dev libxrandr-dev libxfixes-dev libxdamage-dev\
-libxcomposite-dev libxss-dev libxp-dev libxext-dev libxinerama-dev \
-libxkbfile-dev libxtst-dev libxcb1-dev libxcb-shape0-dev\
-libxcb-keysyms1-dev libpulse-dev libsndfile1-dev  libudev-dev\
-libblkid-dev libmount-dev libgstreamer1.0-dev libtiff5-dev libgif-dev\
-curl libssl-dev libspectre-dev libpoppler-dev librsvg2-dev\
-libraw-dev libxinerama-dev git libfribidi-dev libcogl-gles2-dev\
-libbullet-dev libgstreamer-plugins-base1.0-dev libvlc-dev \
-libpoppler-cpp-dev libsystemd-dev
+apt-get install -y \
+make gcc bison flex gawk autoconf automake autopoint bison build-essential ccache check cmake \
+connman connman-dev connman-vpn doxygen flex freeglut3-dev git graphviz graphviz-dev imagemagick \
+libasound2-dev libblkid-dev libblkid-dev libbullet-dev libclang-dev libdbus-1-dev libffi-dev \
+libffi6-dbg libfontconfig1-dev libfreetype6-dev libfribidi-dev libgbm-dev libgif-dev libgles2-mesa-dev \
+libglib2.0-dev libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \
+libharfbuzz-dev libibus-1.0-dev libiconv-hook-dev libinput-dev libjpeg-dev libluajit-5.1-dev libmount-dev \
+libmtdev-dev libopenjpeg-dev libpam0g-dev libpng12-dev libpoppler-dev libpulse-dev libraw-dev librsvg2-dev \
+libscim-dev libsndfile1-dev libspectre-dev libssl-dev libsystemd-dev libtiff5-dev libtool libtool-bin \
+libtorrent-rasterbar-dev libudev-dev libudisks2-dev libunibreak-dev libv8-dev libvlc-dev libwebp-dev \
+libxcb-keysyms1-dev libxcb-shape0-dev libxcomposite-dev libxcursor-dev libxine2-dev libxinerama-dev \
+libxkbcommon-dev libxrandr-dev libxrender-dev libxss-dev libxtst-dev mtdev-tools pkg-config python-dbus \
+python-dev python-distutils-extra python-xdg ragel valgrind wmctrl xserver-xephyr
