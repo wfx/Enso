466c89ad241f29d369ff87db415c483e567f50b3
wfx <wolfgang.morawetz@gmail.com>
2016-10-09 22:37:35 +0200

    run cmd with sudo

--- distribution/debian/build_essential.sh
+++ distribution/debian/build_essential.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install -y \
+sudo apt-get install -y \
 make gcc bison flex gawk autoconf automake autopoint bison build-essential ccache check cmake \
 connman connman-dev connman-vpn doxygen flex freeglut3-dev git graphviz graphviz-dev imagemagick \
 libasound2-dev libblkid-dev libblkid-dev libbullet-dev libclang-dev libdbus-1-dev libffi-dev \
@@ -7,7 +7,7 @@ libffi6-dbg libfontconfig1-dev libfreetype6-dev libfribidi-dev libgbm-dev libgif
 libglib2.0-dev libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \
 libharfbuzz-dev libibus-1.0-dev libiconv-hook-dev libinput-dev libjpeg-dev libluajit-5.1-dev libmount-dev \
 libmtdev-dev libopenjpeg-dev libpam0g-dev libpng12-dev libpoppler-dev libpulse-dev libraw-dev librsvg2-dev \
-libscim-dev libsndfile1-dev libspectre-dev libssl-dev libsystemd-dev libtiff5-dev libtool libtool-bin \
+libscim-dev libsndfile1-dev libspectre-dev libssl-dev libsystemd-dev libsystemd-sysv libtiff5-dev libtool libtool-bin \
 libtorrent-rasterbar-dev libudev-dev libudisks2-dev libunibreak-dev libv8-dev libvlc-dev libwebp-dev \
 libxcb-keysyms1-dev libxcb-shape0-dev libxcomposite-dev libxcursor-dev libxine2-dev libxinerama-dev \
 libxkbcommon-dev libxrandr-dev libxrender-dev libxss-dev libxtst-dev mtdev-tools pkg-config python-dbus \
