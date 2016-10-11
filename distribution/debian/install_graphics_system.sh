1668ac3b1931ee916c7a3713b7a072ed848771af
wfx <wolfgang.morawetz@gmail.com>
2016-10-11 19:35:50 +0200

    minial x (startx)

--- distribution/debian/install_graphics_system.sh
+++ distribution/debian/install_graphics_system.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# minimal x (with startx)
 sudo apt-get update -y
-sudo apt-get install -y x-window-system
+
+sudo apt-get install -y x-window-system dbus-x11 ethtool hdparm pm-utils powermgmt-base vbetool
+#lightdm lightdm-gtk-greeter
+
+cd ~
+echo "exec enlightenment_start" > .xinitrc
