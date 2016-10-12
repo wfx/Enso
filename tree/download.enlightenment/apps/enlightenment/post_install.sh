e175b3b3ec978f78d8ec7a42bd1f0015aaccf730
wfx <wolfgang.morawetz@gmail.com>
2016-10-12 22:54:49 +0200

    test xsessions folder

--- tree/download.enlightenment/apps/enlightenment/post_install.sh
+++ tree/download.enlightenment/apps/enlightenment/post_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # a stupid fix for stupid login manager that don't look in /usr/local
 # DaveMDS quote :)
+
+if [[ ! -d "/usr/share/xsessions/" ]]; then
+  sudo mkdir /usr/share/xsessions/
+fi
 sudo cp data/xsession/enlightenment.desktop /usr/share/xsessions/
