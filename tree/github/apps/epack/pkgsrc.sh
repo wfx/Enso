e987ee237f4b2d0acf0a953c7b9ce67ee187f7a1
wfx <wolfgang.morawetz@gmail.com>
2016-10-13 23:03:06 +0200

    changed git user access

--- tree/github/apps/epack/pkgsrc.sh
+++ tree/github/apps/epack/pkgsrc.sh
@@ -5,7 +5,8 @@ declare -A cfg_prepare
 
 # SETTING ============================================ #
 pkg_source[description]="Epack is a tiny file extractor"  # Short description (optional)
-pkg_source[url]="git@github.com:wfx/epack.git"         # full url (address/filename.extension)
+#pkg_source[url]="git@github.com:wfx/epack.git"         # full url (address/filename.extension)
+pkg_source[url]="https://github.com/wfx/epack.git"
 pkg_source[package]="git"                              # archive -> use tar || git -> git clone TODO: get this information from file
 pkg_source[language]="python"                          # c|python TODO: is it empty then looks for build.sh
 pkg_source[release]=""                                 # optional release number (used for git)
