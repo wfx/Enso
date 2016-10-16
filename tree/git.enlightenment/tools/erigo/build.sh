e69107edca1538d0e1d76b5a405b1b6cfd8433d6
wfx <wolfgang.morawetz@gmail.com>
2016-10-16 23:28:59 +0200

    add build directory check

--- tree/git.enlightenment/tools/erigo/build.sh
+++ tree/git.enlightenment/tools/erigo/build.sh
@@ -1,4 +1,7 @@
-mkdir build
-cd build
-cmake ..
-make
+msg "quote" "Devel, devel, on the wall. Ain't you got no sense at all?"
+if [[ ! -d "${_scriptdir}/${_srcdir}/build" ]]; then
+  run_cmd "mkdir build"
+fi
+run_cmd "cd build"
+run_cmd "cmake .."
+run_cmd "make"
