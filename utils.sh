cf899f2092f231ab105f1f8eec60fbea67cf4c8d
wfx <wolfgang.morawetz@gmail.com>
2016-11-01 14:19:31 +0100

    initial spinner

--- utils.sh
+++ utils.sh
@@ -113,3 +113,17 @@ enso_error() {
     ;;
   esac
 }
+
+spinner()
+{
+  local pid=$!
+  local spin='-\|/'
+
+  local i=0
+  while kill -0 $pid 2>/dev/null
+  do
+    i=$(( (i+1) %4 ))
+    printf "\r${spin:$i:1}"
+    sleep .1
+  done
+}
