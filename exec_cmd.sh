06a63158117e761e83db4f03f9ab08a1cb96d884
wfx <wolfgang.morawetz@gmail.com>
2016-11-01 14:17:46 +0100

    adding more commands

--- exec_cmd.sh
+++ exec_cmd.sh
@@ -23,6 +23,80 @@ exec_ldconfig(){
   sudo ldconfig >> "$stdout" 2> "$stderr" &&  msg "cmd_sudo_passed" || enso_error "1" "$?"
 }
 
+exec_rm_rf(){
+  # arg: $1 directory/to/remove
+  _dir=$1
+  msg "cmd" "rm -rf $_dir"
+  sudo rm -rf "$_dir" >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_mv(){
+  # arg: move from $1 to $2 (new name)
+  _from=$1
+  _to=$2
+  msg "cmd" "mv $_from $_to"
+  mv "${_from}" "${_to}" >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_cd(){
+  # arg: $1 directory/to/change
+  _dir=$1
+  msg "cmd" "cd $_dir"
+  cd ${_dir} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_git_validate_remote(){
+  _url=$1
+  msg "txt" "validity remote git repository..."...
+  msg "cmd" "git ls-remote ${_url}"
+  git ls-remote ${_url} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_git_remote_update(){
+  msg "cmd" "git remote update"
+  git remote update >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_git_clone(){
+  _rel=$1
+  _url=$2
+  if [[ -n ${_rel} ]]; then
+    msg "cmd" "git clone --branch ${_rel} ${_url}"
+    git clone --branch ${_rel} ${_url} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+  else
+    msg "cmd" "git clone ${_url}"
+    git clone ${_url} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+  fi
+}
+
+exec_git_checkout(){
+  _rel=$1
+  if [[ -n ${_rel} ]]; then
+    msg "cmd" "git checkout ${_rel}"
+    git checkout ${_rel} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+  fi
+}
+
+exec_archive_validate_remote(){
+  # check remote archive file access.
+  _url=$1
+  msg "cmd" "wget --spider ${_url}"
+  wget --spider ${_url} -nv >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_archive_download(){
+  _url=$1
+  msg "cmd" "wget -q --show-progress ${_url}"
+  wget -q --show-progress ${_url} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
+exec_archive_extract(){
+  # archive name whitout url
+  _archive=$1
+  msg "cmd" "tar -xf "${_archive}""
+  tar -xf "${_archive}" >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
+}
+
 exec_c_autogen(){
   msg "cmd" "autogen.sh"
   ./autogen.sh ${src_CONFIGURE} >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "1" "$?"
@@ -39,8 +113,8 @@ exec_c_make_clean(){
 }
 
 exec_c_make(){
-  msg "cmd" "make"
-  make >> "$stdout" 2> "$stderr" &&  msg "cmd_passed" || enso_error "10sec" "$?"
+  msg "cmd" "  make"
+  make >> "$stdout" 2> "$stderr" & spinner &&  msg "cmd_passed" || enso_error "10sec" "$?"
 }
 
 exec_c_make_install(){
